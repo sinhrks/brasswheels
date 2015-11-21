a5e14b024586352706bce81e564fc15852a44f22
sinhrks <sinhrks@gmail.com>
2015-11-21 23:17:00 +0900

    Add kmeans

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 extern crate csv;
 extern crate nalgebra;
 extern crate num;
+extern crate rand;
 
 pub mod io;
 pub mod mathfunc;
 
 // single files
 pub mod hclust;
+pub mod kmeans;
 pub mod lm;
 pub mod pca;
